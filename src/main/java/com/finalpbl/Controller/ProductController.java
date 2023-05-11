de201136db2147760c4d1de4bf44a4c01ba0cc17
Loc <leloc70@gmail.com>
2023-05-11 09:57:22 +0700

    product api

--- src/main/java/com/finalpbl/Controller/ProductController.java
+++ src/main/java/com/finalpbl/Controller/ProductController.java
@@ -3,15 +3,17 @@ package com.finalpbl.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.finalpbl.Model.Products;
-import com.finalpbl.Repository.ProductsRepository;
 import com.finalpbl.Service.Products.IProductService;
 
 @RestController
@@ -34,5 +36,16 @@ public class ProductController {
     {
         Products product = iProductService.getProductbyID(id);
         return ResponseEntity.ok(product);
+    }
+
+    @RequestMapping(path = "", method = RequestMethod.POST, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    public ResponseEntity<?> addProduct(@ModelAttribute Products product)
+    {
+        String msg = iProductService.addProduct(product);
+        if(msg.equals("STATUS OK") )
+        {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
