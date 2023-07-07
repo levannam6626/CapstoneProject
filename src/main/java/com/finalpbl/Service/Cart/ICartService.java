05261b32106550a7c31b87a14f2ca99c338a7220
VanNam <vannamka6626@gmail.com>
2023-07-07 15:28:04 +0700

    add edit mutiple cart

--- src/main/java/com/finalpbl/Service/Cart/ICartService.java
+++ src/main/java/com/finalpbl/Service/Cart/ICartService.java
@@ -1,5 +1,7 @@
 package com.finalpbl.Service.Cart;
 
+import java.util.List;
+
 import com.finalpbl.Dto.Cart.AddEditCartDto;
 import com.finalpbl.Dto.Cart.CartDto;
 
@@ -7,6 +9,7 @@ public interface ICartService {
     public CartDto findByUserOrderByCreatedDateDesc(String email);
     public String AddCartItem(AddEditCartDto cartItemDto, String email);
     public String UpdateCart(AddEditCartDto cartItemDto);
+    public String UpdateCartMultiple(List<AddEditCartDto> cartItemDtos);
     public String DeleteCart(long CartId);
     public String DeleteCartByUser(String email);
     public String DeleteOrderdItem(String email);
